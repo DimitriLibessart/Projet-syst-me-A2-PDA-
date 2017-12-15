263786b92c0c791ce24ab666d8907ce66d8fa734
pierre-geeraert <pierre.geeraert@viaces.fr>
2017-12-16 00:59:27 +0100

    commit and check tomorrow for problems

--- script/adress_ip/script.sh
+++ script/adress_ip/script.sh
@@ -24,7 +24,7 @@ digit=$(echo $today |awk '{print $4;}'| sed 's/[^0-9]//g')
 #show hour of log
 ###
 hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
-minuteDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+minuteDigit=$(echo $digit|sed -r 's/.*(.{4})/\1/'|sed -r 's/(.{2}).*/\1/')
 
 ###
 #hour minutes from host
@@ -36,8 +36,8 @@ minuteHost=$(date +"%M")
 ###
 #convert hour:minutes into minutes
 ###
-let "nbrDigit=hourHost*60+minuteHost"
-let "nbrDigit=hourHost*60+minuteHost"
+let "nbrHost=hourHost*60+minuteHost"
+let "nbrDigit=hourDigit*60+minuteDigit"
 
 
 
@@ -59,29 +59,28 @@ specific=$(cat -b access.log |head -n $wanted_line|tail -n 1)
 
 
 
-###
-#test if lines contains today's dates
-###
-today=$(cat access.log |grep $(date  +"%d/$month"))
-
-
-###
-#show hour of log
-###
-#sed -r 's/.*(.{6})/\1/' just-digit.txt|sed -r 's/(.{2}).*/\1/'
 
 
 
 
-#echo $today
-echo $specific
+ligne=2
+number_line=$(echo $digit|wc -l)
 echo $number_line
 
-###
-#rebus
-###
+while [ $ligne -lt $number_line ]
+do
+        $digit=$(echo $today|head -n $number_line|tail -n 1)
+        hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+        minuteDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+        let "nbrHost=hourHost*60+minuteHost"
+        let "nbrDigit=hourDigit*60+minuteDigit"
+        let "result=nbrHost-nbrDigit"
+                if [ $result -le 60 ]
+                        then
+                                echo $today|head -n $number_line|tail -n 1 >> address.csv
 
-#[15/Dec/2017:11:29:01 +0100]
-#echo $(date  +"[%d/$month")
-#echo "[$month"
+
+                fi
+        let 'ligne=ligne+1'
+done
 
--- script/adress_ip/while.sh
+++ script/adress_ip/while.sh
@@ -5,6 +5,17 @@ echo $number_line
 
 while [ $ligne -lt $number_line ]
 do
-        echo "d"
+        $digit=$(echo $today|head -n $number_line|tail -n 1)
+	hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+	minuteDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+	let "nbrHost=hourHost*60+minuteHost"
+	let "nbrDigit=hourDigit*60+minuteDigit"
+	let "result=nbrHost-nbrDigit"
+		if [ $result -le 60 ]
+			then
+        			echo $today|head -n $number_line|tail -n 1 >> address.csv
+
+
+		fi
 	let 'ligne=ligne+1'
 done
