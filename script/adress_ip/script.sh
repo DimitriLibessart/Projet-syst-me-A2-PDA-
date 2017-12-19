9057ff4e2d0e417134cebe58868e46e2d519de8b
pierre-geeraert <pierre.geeraert@viaces.fr>
2017-12-19 11:35:14 +0100

    add script who works fine with number of lines

--- script/adress_ip/script.sh
+++ script/adress_ip/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+
+###################################################################grep date
+
+
 ###
 #month with english version
 ###
@@ -10,89 +14,54 @@ month=$(date -R |awk '{print $3;}')
 ###
 #test if lines contains today's dates
 ###
-today=$(cat access.log |grep $(date  +"%d/$month/%Y"))
-
-
-###
-#convert good log to date
-###
-#digit=$(echo $today |awk '{print $4;}'| sed 's/[^0-9]//g')
+cat access.log |grep $(date  +"%d/$month/%Y")> date.txt
 
 
 
-###
-#show hour of log
-###
-hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
-minuteDigit=$(echo $digit|sed -r 's/.*(.{4})/\1/'|sed -r 's/(.{2}).*/\1/')
-
-###
-#hour minutes from host
-###
-hourHost=$(date +"%H")
-minuteHost=$(date +"%M")
-
-
-###
-#convert hour:minutes into minutes
-###
-let "nbrHost=hourHost*60+minuteHost"
-let "nbrDigit=hourDigit*60+minuteDigit"
-
-
-
-
-
-###
-#display the number of line
-###
-#number_line=$(cat -b access.log |awk '{print $1;}'|tail -n 1)
-number_line=$(cat access.log|grep $(date  +"%d/$month/%Y")|wc -l)
-
-
-
-############################################################remove ?
-wanted_line=12;
-
-###
-#display specific line
-###
-
-specific=$(cat -b access.log |head -n $wanted_line|tail -n 1)
-############################################################
-
-
-
-
-
-
-ligne=1
-
-while [ $ligne -le $number_line ]
+####scan lines per lines
+input="date.txt"
+while IFS= read -r var
 do
 
 
-	digit=$(echo $today |awk '{print $4;}'| sed 's/[^0-9]//g')
+	digit=$(echo $var|awk '{print $4;}'| sed 's/[^0-9]//g')
+	hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+	minuteDigit=$(echo $digit|sed -r 's/.*(.{4})/\1/'|sed -r 's/(.{2}).*/\1/')
+	hourHost=$(date +"%H")
+	minuteHost=$(date +"%M")
+	let "nbrHost=hourHost*60+minuteHost"
+	let "nbrDigit=hourDigit*60+minuteDigit"
+	let "result=nbrHost-nbrDigit"
+	echo "host " $nbrHost
+	echo "digit " $nbrDigit
+	echo "result " $result
 
-        hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
-        minuteDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
-
-
-        let "nbrHost=hourHost*60+minuteHost"
-        let "nbrDigit=hourDigit*60+minuteDigit"
-        let "result=nbrHost-nbrDigit"
-
-               if [ $result -le 60 ]
+	if [ $result -le 60 ]
                         then
 
-                              echo -n $(echo $today|awk '{print $1;}'|head -n $number_line|tail -n 1) >> address.csv
+                              echo -n $(echo $var|awk '{print $1;}') >> address.csv
 
-				ip=$(echo $today|awk '{print $1;}'|head -n $number_line|tail -n 1)
+				ip=$(echo $var|awk '{print $1;}')
 				echo -n "," >>address.csv
 				echo  $(curl -s 'http://ip-api.com/json/'$ip'' |jq -r '.country') >> address.csv
-				#echo "today "$today
+				
 
-                fi
-        let 'ligne=ligne+1'
-	#echo "ligne"$ligne
-done
+        fi
+
+
+
+done < "$input"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
