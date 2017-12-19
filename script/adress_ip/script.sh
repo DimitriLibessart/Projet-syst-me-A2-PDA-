9624ebd9745da2d8b7f2b6cdd05e0cf8b30edb3c
pierre-geeraert <pierre.geeraert@viaces.fr>
2017-12-19 11:54:12 +0100

    add comment :memo:

--- script/adress_ip/script.sh
+++ script/adress_ip/script.sh
@@ -23,26 +23,37 @@ input="date.txt"
 while IFS= read -r var
 do
 
-
+	###print access.log's line in numeric mode 
 	digit=$(echo $var|awk '{print $4;}'| sed 's/[^0-9]//g')
+
+	###extract hour from this digit line
 	hourDigit=$(echo $digit|sed -r 's/.*(.{6})/\1/'|sed -r 's/(.{2}).*/\1/')
+
+	###extract minute from this digit line
 	minuteDigit=$(echo $digit|sed -r 's/.*(.{4})/\1/'|sed -r 's/(.{2}).*/\1/')
+
+	###extract hour from host with same format that digit 
 	hourHost=$(date +"%H")
+
+	###extract minute from host with same format that digit
 	minuteHost=$(date +"%M")
+
+	###convert hour & minute to minutes
 	let "nbrHost=hourHost*60+minuteHost"
 	let "nbrDigit=hourDigit*60+minuteDigit"
 	let "result=nbrHost-nbrDigit"
-	echo "host " $nbrHost
-	echo "digit " $nbrDigit
-	echo "result " $result
+	
+	#if the difference of date lower or egal than 60 minutes (1hour)
 
 	if [ $result -le 60 ]
                         then
-
+				##print ip address in address.csv 
                               echo -n $(echo $var|awk '{print $1;}') >> address.csv
 
 				ip=$(echo $var|awk '{print $1;}')
+				### add comma to respect csv 's syntax
 				echo -n "," >>address.csv
+				###curl and parse from api
 				echo  $(curl -s 'http://ip-api.com/json/'$ip'' |jq -r '.country') >> address.csv
 				
 
